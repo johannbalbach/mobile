8b66945da87f64f8b9b210511eb9ff70575a04e9
nu1ts <123080758+nu1ts@users.noreply.github.com>
2023-05-21 04:44:32 +0700

    bottom bar + console file

--- app/src/main/java/com/example/mobile/columnOfBlocks.kt
+++ app/src/main/java/com/example/mobile/columnOfBlocks.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
