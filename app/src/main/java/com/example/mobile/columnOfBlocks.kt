aa89643ba69350d33ac41d56b9a5810c1b8e431d
nu1ts <123080758+nu1ts@users.noreply.github.com>
2023-05-21 20:08:45 +0700

    new bottom bar

--- app/src/main/java/com/example/mobile/columnOfBlocks.kt
+++ app/src/main/java/com/example/mobile/columnOfBlocks.kt
@@ -10,32 +10,52 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.rounded.Code
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mobile.ui.theme.Orange
 import com.example.mobile.ui.theme.White
 import org.burnoutcrew.reorderable.ReorderableItem
@@ -61,10 +81,27 @@ fun ItemList() {
         topBar = {},
         bottomBar = {
             BottomAppBar(
-                modifier = Modifier.clip(RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)),
-                contentColor = Color(0xFF030204),
-                containerColor = Color(0xFFF6F5F6)
+                contentColor = Color(0xFF818181),
+                containerColor = Color(0xFFF2F2F2)
             ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        onClick = { /* doSomething() */ },
+                        colors = IconButtonDefaults.filledIconButtonColors(
+                            containerColor = White,
+                            contentColor = Color(0xFF616161)
+                        )
+                    ) {
+                        Icon(
+                            Icons.Default.Menu,
+                            contentDescription = null,
+                        )
+                    }
+                    FileMenu()
+                }
                 Row(
                     modifier = Modifier.fillMaxWidth(1f),
                     horizontalArrangement = Arrangement.SpaceAround,
@@ -72,7 +109,13 @@ fun ItemList() {
                 ) {
                     IconButton(onClick = { /* doSomething() */ }) {
                         Icon(
-                            Icons.Default.Menu,
+                            Icons.Outlined.Settings,
+                            contentDescription = null,
+                        )
+                    }
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            Icons.Rounded.Code,
                             contentDescription = null,
                         )
                     }
@@ -85,21 +128,17 @@ fun ItemList() {
                                 blocks.add(ComposeBlock(id, { Output(id, blocks) }))
                             println("variableID: $id")
                         },
-                        shape = CircleShape,
-                        containerColor = Orange,
+                        shape = RoundedCornerShape(16.dp),
+                        containerColor = Color(0xFFBF720F),
                         contentColor = White,
-                        elevation = FloatingActionButtonDefaults.elevation(0.dp)
+                        modifier = Modifier.size(40.dp),
+                        elevation = FloatingActionButtonDefaults.elevation(3.dp)
                     ) {
                         Icon(Icons.Filled.Add, "Localized description")
                     }
-                    IconButton(onClick = { /* doSomething() */ }) {
-                        Icon(
-                            Icons.Rounded.Code,
-                            contentDescription = null,
-                        )
-                    }
                 }
             }
+            Divider(color = Color(0xFFE3E3E3))
         }
     ) {
         val state = rememberReorderableLazyListState(onMove = { from, to ->
@@ -133,6 +172,61 @@ fun ItemList() {
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun FileMenu() {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box() {
+        TextButton(onClick = { expanded = true }) {
+            Text(
+                text = "My Code",
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                fontSize = 21.sp,
+                color = Color.Black
+            )
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = null
+            )
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("Edit") },
+                onClick = { /* Handle edit! */ },
+                leadingIcon = {
+                    Icon(
+                        Icons.Outlined.Edit,
+                        contentDescription = null
+                    )
+                })
+            DropdownMenuItem(
+                text = { Text("Settings") },
+                onClick = { /* Handle settings! */ },
+                leadingIcon = {
+                    Icon(
+                        Icons.Outlined.Settings,
+                        contentDescription = null
+                    )
+                })
+            Divider()
+            DropdownMenuItem(
+                text = { Text("Send Feedback") },
+                onClick = { /* Handle send feedback! */ },
+                leadingIcon = {
+                    Icon(
+                        Icons.Outlined.Email,
+                        contentDescription = null
+                    )
+                },
+                trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
         }
     }
 }
